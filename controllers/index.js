918cf4dba34d591748beded0bfb0cf5ee301a9e0
Balalomba Barandao <barand14@gmail.com>
2024-06-07 13:59:46 -0400

    set up user route

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router()
 const homeRoutes = require("./homeRoutes")
+const apiRoutes = require("./api")
 
 //localhost:3001
-router.use("/", homeRoutes)
-
+router.use("/", homeRoutes)// responsible for view urls and get from db
+router.use("/api", apiRoutes) // responsible for posts, puts, deletes and any changes to db
 
 module.exports = router
